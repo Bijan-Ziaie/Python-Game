e9316192055215e70fa9cddcd8c2dda13dc83784
aadhi0319 <aadhi0319@gmail.com>
2016-05-19 13:37:33 -0500

    Update pythongame.py

--- pythongame.py
+++ pythongame.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import random as r
+import sys
 #import msvcrt as m
-print ("If you want to play, type \nstart()\nBE CAREFUL: IF YOU CLICK RUN, ALL YOUR PROGRESS WILL BE WIPED")
 global health 
 global gold 
 global power
