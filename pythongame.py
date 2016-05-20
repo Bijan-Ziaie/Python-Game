6de382c36281f84c4a66c1ed19b930052d21cdab
Bijan Ziaie <zbijan8@gmail.com>
2016-05-19 20:15:51 -0500

    Update pythongame.py

--- pythongame.py
+++ pythongame.py
@@ -397,7 +397,7 @@ def monster_odd():
     choice = int(raw_input("1.Fight round by round\n2.Skip to end\nChoice: "))
     if choice == 2:
         shdwait = False;
-    elif choice != 1 or choice != 2:
+    elif choice != 1 and choice != 2:
         print("Invalid Input, please enter again")
         monster_even()
     while health > 0 and mon_odd_health > 0:
