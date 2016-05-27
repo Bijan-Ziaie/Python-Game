286cc20c03ca87a963d4c24627ee6281593b7862
Bijan Ziaie <zbijan8@gmail.com>
2016-05-27 12:29:14 -0500

    Update pythongame.py

--- pythongame.py
+++ pythongame.py
@@ -12,7 +12,7 @@ global dragonlive
 dragonlive = True
 shdwait = True
 health = 100
-gold = 500
+gold = 0
 power = 10
 chest = 'None'
 sword = 'None'
@@ -43,28 +43,33 @@ def begin():
     wait()
     choose_path()
 def choose_path():
-    try:
-        global dragonlive
-        if dragonlive == True:
-            if power >= 100:
-                dungeon_answer = str(raw_input("You have enough power to try and fight the DRAGON for his gold. He could have up to 1000 gold coins. \n Be careful, he is very strong....\n Would you like to try and defeat him? : ")).lower()
-                if dungeon_answer == 'yes':
-                    dragon_fight()
-                elif dungeon_answer == 'no':
-                    print ("Okay you can try again the next time you are deciding where to go")
-                    wait()
-                    choose_path_2()
+    global gold
+    global power
+    if gold >= 5000:
+        print("YOU WONNNNNNNNNNNNNNNNNNNNNNN!!!!!!!!!!!!!!")
+    else:
+        try:
+            global dragonlive
+            if dragonlive == True:
+                if power >= 100:
+                    dungeon_answer = str(raw_input("You have enough power to try and fight the DRAGON for his gold. He could have up to 1000 gold coins. \n Be careful, he is very strong....\n Would you like to try and defeat him? : ")).lower()
+                    if dungeon_answer == 'yes':
+                        dragon_fight()
+                    elif dungeon_answer == 'no':
+                        print ("Okay you can try again the next time you are deciding where to go")
+                        wait()
+                        choose_path_2()
+                    else:
+                        print ("That wasnt an answer choice, please answer yes or no")
+                        wait()
+                        choose_path()
                 else:
-                    print ("That wasnt an answer choice, please answer yes or no")
-                    wait()
-                    choose_path()
+                    choose_path_2()
             else:
                 choose_path_2()
-        else:
-            choose_path_2()
-    except:
-        print("Incorrect Input")
-        choose_path()
+        except:
+            print("Incorrect Input")
+            choose_path()
 def choose_path_2():
     try:
         answer = str(raw_input("Where would you like to go now?\nIn 'town',you can buy items which increase your power or pay a fee to heal yourself\nIn 'forest', you can battle monsters and take their gold or possibly discover treasure...\nYour Choice: ")).lower()
